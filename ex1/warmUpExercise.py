679021dc9e53c5e415c9afa94292355f130968ca
AngieApproved <34202774+AngieApproved@users.noreply.github.com>
2018-03-27 10:19:41 -0500

    Updated to solution

--- ex1/warmUpExercise.py
+++ ex1/warmUpExercise.py
@@ -2,7 +2,7 @@ from numpy import eye
 
 def warmUpExercise():
     """ an example function that returns the 5x5 identity matrix
-   """
+    """
 
 # ============= YOUR CODE HERE ==============
 # Instructions: Return the 5x5 identity matrix
@@ -10,6 +10,6 @@ def warmUpExercise():
 #               represent the return values (at the top of the file)
 #               and then set them accordingly.
    
-    return eye(5)
+    return eye(5);
 
 # ===========================================
