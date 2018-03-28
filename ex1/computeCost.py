fbece150fcd86b6c0659c14bede5d4599f58dbb5
AngieApproved <34202774+AngieApproved@users.noreply.github.com>
2018-03-28 16:15:59 -0500

    Updated

--- ex1/computeCost.py
+++ ex1/computeCost.py
@@ -1,17 +1,37 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def computeCost(X, y, theta):
     """
        computes the cost of using theta as the parameter for linear 
        regression to fit the data points in X and y
     """
-    m = y.size
-    J = 0
+   
+    data = np.loadtxt('ex1data1.txt', delimiter=',')
+
+
+
+    m = y.size      #number of training samples
+    X = data[:, 0]
+    y = data[:, 1]
+    
+    predictions = X.dot(theta).flatten()
+    sqErrors = (predictions - y) **2
+    
 
 # ====================== YOUR CODE HERE ======================
 # Instructions: Compute the cost of a particular choice of theta
 #               You should set J to the cost.
 
+   
+    it = np.ones(shape = (m, 2))
+    it[:, 1] = X
+    
+    theta = np.zeros(shape = (2, 1))
+    iterations = 1500
+    alpha = 0.01
+    
+    J = (1.0 / (2 * m)) * sqErrors.sum()
 
 # =========================================================================
 
